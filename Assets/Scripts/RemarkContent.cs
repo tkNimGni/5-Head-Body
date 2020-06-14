345f5df6b5c21d3293fa3ee8ba55d1b6e4815f8f
wryou <wryou@toydea.com>
2020-06-14 18:05:26 +0900

    build settings

---
+++ Assets/Scripts/RemarkContent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RemarkContent : MonoBehaviour
+{
+    void Start()
+    {
+        System.String key = "key";
+        InputField content = gameObject.GetComponent<InputField>();
+        content.onEndEdit.AddListener(delegate { PlayerPrefs.SetString(key, content.textComponent.text); });
+        if (PlayerPrefs.HasKey(key)) content.text = PlayerPrefs.GetString(key);
+    }
+}
